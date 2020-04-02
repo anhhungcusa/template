11381cce923c284a045d7b3fba971214b4fd69d2
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-04-02 08:38:46 +0700

    add animation for header and dropdown table menu

--- zingmp3-UI/js/pages/home.js
+++ zingmp3-UI/js/pages/home.js
@@ -92,3 +92,29 @@ albumSliders.forEach((e, index) => {
 })
 
 
+// real-time chart [highcharts]
+document.addEventListener('DOMContentLoaded', function () {
+    var myChart = Highcharts.chart('chart-container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
