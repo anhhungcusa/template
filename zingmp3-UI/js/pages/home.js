898a4202562383b86b6684101c9d9a8262ca4091
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-04-03 11:51:45 +0700

    add realtime-chart using highcharts

--- zingmp3-UI/js/pages/home.js
+++ zingmp3-UI/js/pages/home.js
@@ -61,7 +61,7 @@ albumSliders.forEach((e, index) => {
         const albumSlider = $(e)
         const controller = slideControllerCollection[index]
         const { 0: prevControl, 1: nextControl } = controller && controller.children || {}
-        const margin =  e.dataset.margin ? +e.dataset.margin : 20
+        const margin = e.dataset.margin ? +e.dataset.margin : 20
         let addition = {}
         if (e.dataset.auto == '1') {
             addition = {
@@ -91,30 +91,125 @@ albumSliders.forEach((e, index) => {
 
 })
 
-
 // real-time chart [highcharts]
 document.addEventListener('DOMContentLoaded', function () {
     var myChart = Highcharts.chart('chart-container', {
         chart: {
-            type: 'bar'
+            type: 'line',
+            width: 600,
+            height: 350,
+            backgroundColor: null,
+            className: 'chart-container',
+            marginLeft: 20,
         },
         title: {
-            text: 'Fruit Consumption'
+            text: ''
+        },
+        legend: {
+            enabled: false
         },
         xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
+            // categories: [9, 11, 13, 15, 17, 19, 21, 23, 1, 3, 5, 7],
+            tickInterval: 3, // one week
+            tickWidth: 0,
+            lineColor: 'transparent',
+            crosshair: {
+                width: 1,
+            },
+            labels: {
+                style: {
+                    color: '#a2a2a2',
+                    fontSize: '12px'
+                }
+            }
         },
         yAxis: {
+            visible: false,
             title: {
-                text: 'Fruit eaten'
+                text: ''
+            },
+
+            // type: 'linear',
+            gridLineWidth: 0
+        },
+        plotOptions: {
+            series: {
+                cursor: 'pointer',
+                marker: {
+                    fillColor: '#fff',
+                    symbol: 'circle',
+                    lineWidth: 2,
+                    enabled: false
+                },
+                events: {
+                    mouseOver: function () {
+                        // console.log('mouseOver', this.options.data)
+                        
+                        this.update({ marker: { enabled: true } })
+                        const color = arguments[0].target.color;
+                        this.chart.update({
+                            xAxis: {crosshair: {color}},
+                            tooltip: {backgroundColor: color}
+                        })
+                        const t = document.querySelector(`#${this.name}`)
+                        t.classList.add('active')
+                    },
+                    mouseOut: function () {
+                        // this.options.marker.enabled = false
+                        this.update({ marker: { enabled: false } })
+                        const t = document.querySelector(`#${this.name}`)
+                        t.classList.remove('active')
+                    }
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return `Thời gian: ${('0'+this.x).slice(-2)}:00`
+            },
+            padding: 5,
+            style: {
+                color: '#fff',
             }
         },
         series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
+            name: 't1',
+            marker: {
+                lineColor: '#4a8fe1',
+            },
+            color: '#4a8fe1',
+            data: [31, 14, 27, 22, 39, 0, 35, 22, 30, 22, 34, 20, 4, 11, 22, 34, 40, 32, 31, 32, 41, 10, 3, 48]
         }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
+            name: 't2',
+            marker: {
+                lineColor: '#d94d4f',
+            },
+            color: '#d94d4f',
+            data: [41, 39, 57, 34, 57, 98, 60, 60, 28, 43, 58, 42, 96, 57, 37, 46, 50, 47, 43, 45, 17, 90, 82, 37]
+        },{
+            name: 't3',
+            marker: {
+                lineColor: '#4ed7ba',
+            },
+            color: '#4ed7ba',
+            data: [28, 47, 16, 44, 4, 2, 5, 18, 42, 35, 8, 38, 0, 32, 41, 20, 10, 21, 26, 23, 42, 0, 15, 15],
+        }, 
+        ]
     });
+
+    const tCollections = document.getElementsByClassName('t')
+    const onMouseEnter = (e) => {
+        const id = e.target.id
+        myChart.series[id.slice(-1) - 1 ].update({ marker: { enabled: true } })
+    }
+
+    const onMouseLeave = (e) => {
+        const id = e.target.id
+        myChart.series[id.slice(-1) - 1 ].update({ marker: { enabled: false } })
+    }
+    for (const element of tCollections) {
+        element.addEventListener('mouseenter', onMouseEnter)
+        element.addEventListener('mouseleave', onMouseLeave)
+        
+    }
 });
